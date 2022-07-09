bbbdd63dc0d266d5cc0fb0d0ee514828dfd1a8eb
Belchior Oliveira <belchior@outlook.com>
2022-07-09 19:19:04 -0300

    renamed test scripts

--- scripts/watch_test.sh
+++ scripts/watch_test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # cargo watch -w ./src -w ./tests -x 'test --features postgresql -- --nocapture --color always'
-cargo watch -w ./src -w ./tests -x 'test --features postgresql'
+cargo watch -w ./src -w ./tests -x 'test --features postgresql --test cargo_feature_postgresql'
