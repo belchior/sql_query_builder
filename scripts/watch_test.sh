242e8e037d8fc6eb4848aa500b3228d4218d99ca
Belchior Oliveira <belchior@outlook.com>
2022-07-26 08:36:29 -0300

    project setup improvements

--- scripts/watch_test.sh
+++ scripts/watch_test.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# Prerequisites
+# cargo install cargo-watch
+
 # cargo watch -w ./src -w ./tests -x 'test --features postgresql -- --nocapture --color always'
 cargo watch -w ./src -w ./tests -x 'test --features postgresql'
