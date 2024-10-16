39a1b89e214fd1410a9eaab4566a38dd54fe30df
Belchior Oliveira <belchior@outlook.com>
2024-10-16 15:10:48 -0300

    fix accumulator methods of values command

--- tests/clause_values_spec.rs
+++ tests/clause_values_spec.rs
@@ -110,6 +110,18 @@ mod values_command {
   }
 
   #[test]
+  fn method_values_should_not_accumulate_values_when_table_name_is_empty() {
+    let query = sql::Values::new()
+      .values("")
+      .values("('foo', 'Foo')")
+      .values("")
+      .as_string();
+    let expected_query = "VALUES ('foo', 'Foo')";
+
+    assert_eq!(query, expected_query);
+  }
+
+  #[test]
   fn method_values_should_trim_space_of_the_argument() {
     let query = sql::Values::new().values("   ('Bar')  ").as_string();
     let expected_query = "VALUES ('Bar')";
